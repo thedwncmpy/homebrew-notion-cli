d77f74f421ca053b4dc5e3641803a4c79763c5ea
thedwncmpy <158208630+thedwncmpy@users.noreply.github.com>
2026-06-20 23:29:30 +0000

    chore: bump ns formula to v0.2.9

--- Formula/ns.rb
+++ Formula/ns.rb
@@ -3,8 +3,8 @@
 class Ns < Formula
   desc "Notion markdown sync CLI"
   homepage "https://github.com/thedwncmpy/ns-cli"
-  url "https://github.com/thedwncmpy/ns-cli/archive/refs/tags/v0.2.8.tar.gz"
-  sha256 "566e36bf64b18b9017859f56c971db1d254c34d256fb1d25ce81c5ce1f9c04ce"
+  url "https://github.com/thedwncmpy/ns-cli/archive/refs/tags/v0.2.9.tar.gz"
+  sha256 "63f4e5315a1bd887b78e1dadfd5ef809864937b72d1f709b375f00e2b216c74c"
   license "MIT"
 
   head "https://github.com/thedwncmpy/notion-cli.git", branch: "main"
